c4a2864825c7bb3f7b9a548dcf4db98fe5e71127
ligerandbear <carol@thewiseoptimist.net>
2016-04-09 11:13:33 -0700

    copy pasted portion of changed updatePosition funtion

--- views/js/main.js
+++ views/js/main.js
@@ -501,11 +501,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-  
+
   var items = document.querySelectorAll('.mover');
-  var phaseCalc = Math.sin((document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
-    var phase = phaseCalc + (i % 5));
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
